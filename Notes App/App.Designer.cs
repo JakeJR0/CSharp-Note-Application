13f0333204eef42267a0f2a3562daef48b726ff1
Jake James-Robinson <1453194@stu.tmc.ac.uk>
2022-05-10 10:28:55 +0100

    Add project files.

---
+++ Notes App/App.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Notes_App
+{
+    partial class Notes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NoteOpener = new System.Windows.Forms.OpenFileDialog();
+            this.SaveNote = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.NoteInput = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // NoteOpener
+            // 
+            this.NoteOpener.DefaultExt = "txt";
+            this.NoteOpener.FileName = "Untitled";
+            this.NoteOpener.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            this.NoteOpener.Title = "Select your notes";
+            // 
+            // SaveNote
+            // 
+            this.SaveNote.DefaultExt = "txt";
+            this.SaveNote.FileName = "Untitled";
+            this.SaveNote.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            this.SaveNote.Title = "Save your notes";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(489, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFileToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openFileToolStripMenuItem
+            // 
+            this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            this.openFileToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openFileToolStripMenuItem.Text = "Open File";
+            this.openFileToolStripMenuItem.Click += new System.EventHandler(this.openFileToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // NoteInput
+            // 
+            this.NoteInput.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.NoteInput.Location = new System.Drawing.Point(13, 63);
+            this.NoteInput.Name = "NoteInput";
+            this.NoteInput.Size = new System.Drawing.Size(464, 377);
+            this.NoteInput.TabIndex = 1;
+            this.NoteInput.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.Color.DarkGray;
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.richTextBox2.Location = new System.Drawing.Point(13, 28);
+            this.richTextBox2.Multiline = false;
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(464, 29);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "";
+            // 
+            // Notes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(489, 452);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.NoteInput);
+            this.Controls.Add(this.menuStrip1);
+            this.ForeColor = System.Drawing.Color.Cornsilk;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Notes";
+            this.Text = "Sharp Notes";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog NoteOpener;
+        private System.Windows.Forms.SaveFileDialog SaveNote;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox NoteInput;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ Notes App/App.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Notes_App
+{
+    public partial class Notes : Form
+    {
+        string fileName = "";
+        
+        public Notes()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveFile(string file= "")
+        {
+            if (file == "")
+            {
+                if (SaveNote.ShowDialog() == DialogResult.OK)
+                {
+                    file = SaveNote.FileName;
+                    fileName = SaveNote.FileName;
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            using (StreamWriter sw = new StreamWriter(file))
+            {
+                sw.Write(NoteInput.Text);
+                sw.Close();
+            }
+        }
+
+        private void OpenFile()
+        {
+            
+            if (NoteOpener.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamReader sr = new StreamReader(NoteOpener.FileName))
+                {
+                    string output = sr.ReadToEnd();
+                    NoteInput.Text = output;
+                    fileName = NoteOpener.FileName;
+                    sr.Close();
+                }
+            }
+        }
+
+        private void openFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFile();
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFile();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFile(fileName);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
